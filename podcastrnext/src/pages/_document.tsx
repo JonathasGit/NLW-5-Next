a07c530d64661077a494be6aa0922ebcaf428ae0
Jonathas <jonathas-25@hotmail.com>
2021-04-23 00:50:20 -0300

    #2 erro ao subir projeto
    Erro no code agora.

--- podcastrnext/src/pages/_document.tsx
+++ podcastrnext/src/pages/_document.tsx
@@ -1,21 +1,21 @@
 import Document, {Html, Head, Main, NextScript } from 'next/document';
 
 
-export default class MyDocument extends Document{ // Só pode classe pela documentação do next 
+export default class MyDocument extends Document{  
     render() {
         return(
-            <html>
-                <head>
+            <Html>
+                <Head />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" rel="stylesheet" /> 
 
-                </head>
+                
                 <body>
                     <Main />
                     <NextScript />
                 </body>
 
-            </html>
+            </Html>
         );       
         
     }
