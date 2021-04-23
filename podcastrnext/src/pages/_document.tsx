c859da277646c354e527475a801f276dee246b51
Jonathas <jonathas-25@hotmail.com>
2021-04-22 22:55:55 -0300

    #2 configurando as  fontes
    Criando umas fontes. Arquivo document

---
+++ podcastrnext/src/pages/_document.tsx
@@ -0,0 +1,23 @@
+import Document, {Html, Head, Main, NextScript } from 'next/document';
+
+
+export default class MyDocument extends Document{ // Só pode classe pela documentação do next 
+    render() {
+        return(
+            <html>
+                <head>
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" rel="stylesheet" /> 
+
+                </head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+
+            </html>
+        );       
+        
+    }
+
+}
